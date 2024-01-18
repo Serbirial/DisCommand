6b416d161141648db46934ddcd02c919ee768698
Koya <39264658+Serbirial@users.noreply.github.com>
2024-01-18 08:50:32 -0500

    Add typing

--- discommand/entry/inject.py
+++ discommand/entry/inject.py
@@ -6,7 +6,7 @@ from discord import (
     AutoShardedClient
 )
 
-def inject_into_bot(bot):
+def inject_into_bot(bot: Client | AutoShardedClient):
     if not issubclass(bot.__class__, Client) or not issubclass(bot.__class__, AutoShardedClient):
         raise TypeError("Bot is not of type discord.Client or discord.AutoShardedClient (ext.commands.Bot or ext.commands.AutoShardedBot are not supported as they implement commands themselves.)")
     
