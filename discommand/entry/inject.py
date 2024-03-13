abfb6a4080d24850281c3b2b74b1dfe4ff61b758
Koya <39264658+Serbirial@users.noreply.github.com>
2024-03-13 13:11:44 -0400

    Fix inject.

--- discommand/entry/inject.py
+++ discommand/entry/inject.py
@@ -7,7 +7,7 @@ from discord import (
 )
 
 def inject_into_bot(bot: Client | AutoShardedClient):
-    if not issubclass(bot.__class__, Client) or not issubclass(bot.__class__, AutoShardedClient):
+    if not issubclass(bot, Client) or not issubclass(bot, AutoShardedClient):
         raise TypeError("Bot is not of type discord.Client or discord.AutoShardedClient (ext.commands.Bot or ext.commands.AutoShardedBot are not supported as they implement commands themselves.)")
     
     bot.cog_manager = CogManager(bot)
