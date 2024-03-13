57eb9d6122b3efcd6e1c29c27e0ab01e8c0384e8
Koya <39264658+Serbirial@users.noreply.github.com>
2024-03-13 14:56:52 -0400

    Update shard.py

--- discommand/sharding/shard.py
+++ discommand/sharding/shard.py
@@ -1,4 +1,5 @@
 from discord import AutoShardedClient
+from threading import Thread
 
 class Shard:
 	def __init__(self, id: int, shard_range: list[int, int], client: AutoShardedClient) -> None:
@@ -7,9 +8,15 @@ class Shard:
 		self.shard_range = shard_range
 		self.client = client
 
+		self.login_thread = Thread(target=self.login, args=)
+
+
 	def init_shard(self, shard_count):
 		self.client.shard_count = shard_count 
 		self.client.shard_ids = self.shard_range
 
-	def login(self, token):
+	def _actual_login(self, token):
 		self.client.run(token)
+
+	def login(self, token):
+		self.login_thread = Thread(target=self._actual_login, args=(token,))
